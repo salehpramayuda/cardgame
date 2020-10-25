e89d9d5a1cf6556d028bd5a95739a635e8ae2bf3
DESKTOP-FH8T1VR\saleh <salehpramayuda@gmail.com>
2020-10-25 23:05:29 +0100

    add insert function, throw_away function to handstack

--- handstack.cpp
+++ handstack.cpp
@@ -8,8 +8,16 @@ void Handstack::draw(Cardstack others){
 }
 
 void Handstack::insert(Card* temp){
-
+    this->hand.push_back(temp);
 }
 
-void Handstack::throw_away(unsigned int index, Cardstack){
+void Handstack::throw_away(unsigned int index, Cardstack other){
+    //throw away the i-th card in hand (starts from 0)
+    Card* temp = this->hand[index];                 //copy card into a pointer
+    this->hand.erase(this->hand.begin()+index);     //remove card from vector
+    other.insert(temp);                             //insert card into stack
+}
+
+void list_card(){
+    
 }
